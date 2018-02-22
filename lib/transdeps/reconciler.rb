3a4e0c2dd8c4d4ee1b6ae8c0e64443f18ea8b29f
Arturo Pie <arturop@nulogy.com>
2018-02-22 15:23:41 -0500

    2: ignores files when collecting all components in  directory

--- lib/transdeps/reconciler.rb
+++ lib/transdeps/reconciler.rb
@@ -9,16 +9,28 @@ module Transdeps
 
     def call(component_dir, project_dir)
       project_specs = specs_for(project_dir)
-      all_component_specs = component_dir.children.map { |dir| specs_for(dir) }
-      all_component_specs.flat_map do |component_specs|
-        project_specs - component_specs
-      end.compact
+      all_component_specs = all_component_specs(component_dir)
+
+      spec_differences(project_specs, all_component_specs)
     end
 
     private
 
     def specs_for(dir)
       factory.call(dir)
+    end
+
+    def all_component_specs(component_dir)
+      component_dir
+        .children
+        .select { |filename| File.directory?(filename) }
+        .map { |dir| specs_for(dir) }
+    end
+
+    def spec_differences(project_specs, all_component_specs)
+      all_component_specs.flat_map do |component_specs|
+        project_specs - component_specs
+      end.compact
     end
   end
 end
